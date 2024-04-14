adf051b0799398ea29959d45392b20e7e1849c47
H-Bharmal <hb.hamzabharmal@gmail.com>
2024-04-15 01:57:13 +0530

    search controller

---
+++ Backend/src/controllers/search.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../utils/asyncHandler";
+
+// TODO : Add feature for filters by year, page, author, publication etc.
+const searchBooksAvailableInDB = asyncHandler( async(req, res)=>{
+    // Anything related to book
+    // Title, isbn, categories, description, author, publication
+
+})
