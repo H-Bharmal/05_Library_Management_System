5dde48c0ccd11dbd5f97bc6262bd2eab1e50fcf0
H-Bharmal <hb.hamzabharmal@gmail.com>
2024-04-27 03:18:47 +0530

    Fix api_domain

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const API_DOMAIN="https://zero5-library-management-system.onrender.com";
-// export const API_DOMAIN="http://localhost:8000";
+export const API_DOMAIN="https://zero5-library-management-system.onrender.com/api/v1";
+// export const API_DOMAIN="http://localhost:8000/api/v1";
